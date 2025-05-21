7fe0d552d5e7fb6ef8bde4f9bdad37c3e8a7038f
Joakimglodedata <joakim.glodedata@gmail.com>
2025-05-21 14:09:45 +0200

    mye

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Hello!');
+console.log("kfnf")
+
+// meny.html
+const pizzaCardContainer = document.querySelector("#pizzaCardContainer")
+console.log(pizzaCardContainer)
+
+const pizzaCard = document.createElement("div")
+pizzaCard.classList.add("pizzaCard")
+
+const pizzaCardImg = document.createElement("img")
+pizzaCardImg.classList.add("pizzaCardImg")  
+pizzaCardImg.src = ""
+pizzaCardImg.alt = "Bilde av pizza"
+
+const pizzaCardTitle = document.createElement("h3")
+pizzaCardTitle.classList.add("pizzaCardTitle")
+pizzaCardTitle.textContent = "Pizza navn"
+
+const pizzaCardDescription = document.createElement("p")
+pizzaCardDescription.classList.add("pizzaCardDescription")
+pizzaCardDescription.textContent = "Pizza beskrivelse"
+
+const pizzaCardPrize = document.createElement("p")
+pizzaCardPrize.classList.add("pizzaCardPrize")
+pizzaCardPrize.textContent = "Pizza pris"
+
+pizzaCard.appendChild(pizzaCardImg)
+pizzaCard.appendChild(pizzaCardTitle)
+pizzaCard.appendChild(pizzaCardDescription)
+pizzaCard.appendChild(pizzaCardPrize)
+
+Array.from({ length: 12 }, () => pizzaCardContainer.appendChild(pizzaCard.cloneNode(true)));
